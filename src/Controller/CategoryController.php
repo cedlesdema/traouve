4182893a37f04453f4cab8a03c666589da2d2874
cedlesdema <cedlesdema@protonmail.com>
2018-11-30 00:19:12 +0100

    dernier commit pour presentation

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -3,7 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Category;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Traobject;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CategoryController extends BaseController
@@ -17,6 +18,18 @@ class CategoryController extends BaseController
 
         return $this->render('category/footercategory.html.twig', [
             'categories' => $categories,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="category_show", methods="GET")
+     */
+    public function show(Category $category): Response
+    {
+        $category = $this->getDoctrine()->getRepository(Traobject::class)->findBy(["id" => Category::class]);
+
+        return $this->render('category/show.html.twig', [
+            'category' => $category
         ]);
     }
 }
